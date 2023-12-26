c1b5efe03d04a0a682b70ae6bafc913c09e41508
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2023-12-26 17:45:57 +0100

    Associative Array

--- index.php
+++ index.php
@@ -26,9 +26,43 @@
     <ul>
         <?php
             foreach ($names as $name) {
-                echo "<li>$name</li>";
+                echo "<li>$name</li> <br/>";
             }
         ?>
+
+        <!-- A shorter syntax with HTML in between-->
+        <?php foreach ($names as $name) : ?>
+        <li>
+            <?php echo $name; ?>
+        </li>
+        <?php endforeach; ?>
+
+        <!-- Associative Array -->
+        <?php
+            $people = [
+                [
+                    "name" => "John",
+                    "gender" => "Male",
+                    "age" => 30,
+                ],
+                [
+                    "name" => "Doe",
+                    "gender" => "Male",
+                    "age" => 20,
+                ],
+                [
+                    "name" => "Jane",
+                    "gender" => "Female",
+                    "age" => 15,
+                ],
+            ];
+        ?>
+
+        <?php foreach ($people as $person) : ?>
+        <span>Name : <?php echo  $person["name"] ?></span> <br>
+        <span>Age : <?php echo  $person["age"] ?></span> <br>
+        <span>Age : <?php echo  $person["gender"] ?></span> <br> <br>
+        <?php endforeach; ?>
     </ul>
 
 </body>
