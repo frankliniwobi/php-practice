b8cda8a163a146039fb0944f35293d4fe6dab2bb
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2023-12-26 19:11:24 +0100

    Separate view from logic

--- index.php
+++ index.php
@@ -1,72 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Associative Array -->
+<?php
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
+$people = [
+    [
+        "name" => "John",
+        "gender" => "Male",
+        "age" => 30,
+    ],
+    [
+        "name" => "Doe",
+        "gender" => "Male",
+        "age" => 20,
+    ],
+    [
+        "name" => "Jane",
+        "gender" => "Female",
+        "age" => 15,
+    ],
+];
 
-<body>
-    <h1>
-        <?php
-        $greeting = "Hello";
-        echo "$greeting World PHP 2!";
-      ?>
-    </h1>
+$filtered = array_filter($people, function($data) {
+    return $data['age'] >= 20;
+});
 
-    <?php
-        $names = [
-            "John",
-            "Doe",
-            "Jane",
-        ];
-    ?>
-
-    <ul>
-        <?php
-            foreach ($names as $name) {
-                echo "<li>$name</li> <br/>";
-            }
-        ?>
-
-        <!-- A shorter syntax with HTML in between-->
-        <?php foreach ($names as $name) : ?>
-        <li>
-            <?php echo $name; ?>
-        </li>
-        <?php endforeach; ?>
-
-        <!-- Associative Array -->
-        <?php
-            $people = [
-                [
-                    "name" => "John",
-                    "gender" => "Male",
-                    "age" => 30,
-                ],
-                [
-                    "name" => "Doe",
-                    "gender" => "Male",
-                    "age" => 20,
-                ],
-                [
-                    "name" => "Jane",
-                    "gender" => "Female",
-                    "age" => 15,
-                ],
-            ];
-
-            $filtered = array_filter($people, function($data) {
-                return $data['age'] >= 20;
-            });
-
-            foreach ($filtered as $data) {
-                echo "me";
-            }
-        ?>
-    </ul>
-
-</body>
-
-</html>
+require "index.view.php";
