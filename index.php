1b09175ccbf8798c18578c18b8c81b4e5230789e
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2023-12-27 07:04:50 +0100

    Connect to a MySql database

--- index.php
+++ index.php
@@ -2,4 +2,20 @@
 
 require "functions.php";
 
-require "router.php";
+// require "router.php";
+
+// Connect to the MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=demo;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// dd($post);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
