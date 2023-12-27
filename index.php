16a147640095fe71d305c4fb4a484e615f07a976
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2023-12-27 07:19:06 +0100

    Refactor conection to a database class

--- index.php
+++ index.php
@@ -4,18 +4,9 @@ require "functions.php";
 
 // require "router.php";
 
-// Connect to the MySQL database.
-$dsn = "mysql:host=localhost;port=3306;dbname=demo;user=root;charset=utf8mb4";
+require 'Database.php';
 
-$pdo = new PDO($dsn);
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// dd($post);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
