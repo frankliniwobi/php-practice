05e08588dc4be0e3816d0dcdc6f28b6904ce9f3c
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2023-12-27 04:50:09 +0100

    Made the barner dynamic

--- views/about.view.php
+++ views/about.view.php
@@ -13,11 +13,8 @@
 
         <?php require "includes/nav.php" ?>
 
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-            </div>
-        </header>
+        <?php require "includes/barner.php" ?>
+
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 <p>Welcome to the About page.</p>
--- views/contact.view.php
+++ views/contact.view.php
@@ -9,23 +9,12 @@
 </head>
 
 <body class="h-full">
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
     <div class="min-h-full">
 
         <?php require "includes/nav.php" ?>
 
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-            </div>
-        </header>
+        <?php require "includes/barner.php" ?>
+
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 <p>Welcome to the Contact page.</p>
