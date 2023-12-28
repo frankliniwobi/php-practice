0357139bfddd448ed5f087492f01689a6f57d896
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2023-12-28 14:59:28 +0100

    Automatically load classes

--- public/index.php
+++ public/index.php
@@ -4,10 +4,13 @@ const BASE_PATH = __DIR__ . "/../";
 
 require BASE_PATH . 'functions.php';
 
-require base_path('Database.php');
-
-require base_path('Validator.php');
-
-require base_path('Response.php');
+/**
+ * To avoid requiring every class we may need
+ * later on, let's allow php to autoload
+ * the classes automatically.
+ */
+spl_autoload_register(function($class) {
+    require base_path($class . '.php') ;
+});
 
 require base_path('router.php');
