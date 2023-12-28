318a630c2d8d9f6a5861c38cd902abe1031b650f
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2023-12-28 10:34:50 +0100

    Create new post

--- controllers/post-create.php
+++ controllers/post-create.php
@@ -1,23 +1,47 @@
 <?php
 
-$heading = "Create Post";
+$heading = 'Create Post';
 
 $config = require('config.php');
+$db = new Database($config['database']);
 
-if ($_SERVER["REQUEST_METHOD"] === "POST" ) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' ) {
 
-    $db = new Database($config['database']);
+    $errors = [];
 
-    $id = 1;
+    if (strlen($_POST['title']) > 100 ) {
+        $errors['title'] = 'The title cannot be more than 100 characters';
+    }
 
-    $query = "INSERT INTO `posts` (`title`, `body`, `user_id`) VALUES (:title, :body, :user_id) ";
+    if (strlen($_POST['title']) === 0 ) {
+        $errors['title'] = 'The title is required';
+    }
 
-    $posts = $db->query($query, [
-        ':title' => $_POST['title'],
-        ':body' => $_POST['body'],
-        ':user_id' => $id,
-    ])->get();
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'The body is required';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body cannot be more than 1000 characters';
+    }
+
+    if (empty($errors)) {
+
+        $id = 1;
+
+        $query = 'INSERT INTO `posts` (`title`, `body`, `user_id`) VALUES (:title, :body, :user_id) ';
+
+        $posts = $db->query($query, [
+            ':title' => $_POST['title'],
+            ':body' => $_POST['body'],
+            ':user_id' => $id,
+        ])->get();
+
+        header("Location: /posts/create?success=Post+created+successfully!");
+        exit;
+
+    }
 
 }
 
-require "views/post-create.view.php";
+require 'views/post-create.view.php';
--- views/post-create.view.php
+++ views/post-create.view.php
@@ -8,6 +8,12 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <div class="md:grid md:grid-cols-3 md:gap-6">
             <div class="mt-5 md:col-span-2 md:mt-0">
+                <?php if(isset($_GET['success'])) : ?>
+                <div class="my-5 p-5 bg-green-300 rounded-md ">
+                    <span class="font-bold text-black"><?= $_GET['success'] ?></span>
+                </div>
+                <?php endif; ?>
+
                 <form method="POST">
                     <div class="shadow sm:overflow-hidden sm:rounded-md">
 
@@ -16,9 +22,14 @@
                                 <label for="body" class="block text-sm font-medium text-gray-700">Title</label>
 
                                 <div class="mt-1">
-                                    <input id="title" name="title"
+                                    <input id="title" name="title" value="<?php echo $_POST['title'] ?? '' ?>"
                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"></input>
                                 </div>
+
+
+                                <small class="text-red-500 mt-2"><?= $errors['title'] ?? '' ?></small>
+
+
                             </div>
 
                             <div>
@@ -27,8 +38,12 @@
                                 <div class="mt-1">
                                     <textarea id="body" name="body" rows="3"
                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                        placeholder="Here's an idea for a post..."></textarea>
+                                        placeholder="Here's an idea for a post..."><?php echo $_POST['body'] ?? '' ?></textarea>
                                 </div>
+
+
+                                <small class="text-red-500 mt-2"><?= $errors['body'] ?? '' ?></small>
+
                             </div>
                         </div>
 
