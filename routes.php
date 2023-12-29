e75d1e08dca57243f1bc787a7fbebdb711b6430f
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2023-12-29 02:05:34 +0100

    Impliment route middleware to protect specific routes

--- routes.php
+++ routes.php
@@ -4,13 +4,13 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/posts', 'controllers/posts/index.php');
-$router->post('/posts', 'controllers/posts/store.php');
-$router->get('/post', 'controllers/posts/show.php');
-$router->put('/post', 'controllers/posts/update.php');
-$router->delete('/post', 'controllers/posts/destroy.php');
-$router->get('/posts/create', 'controllers/posts/create.php');
-$router->get('/post/edit', 'controllers/posts/edit.php');
+$router->get('/posts', 'controllers/posts/index.php')->middleware('auth');
+$router->post('/posts', 'controllers/posts/store.php')->middleware('auth');
+$router->get('/post', 'controllers/posts/show.php')->middleware('auth');
+$router->put('/post', 'controllers/posts/update.php')->middleware('auth');
+$router->delete('/post', 'controllers/posts/destroy.php')->middleware('auth');
+$router->get('/posts/create', 'controllers/posts/create.php')->middleware('auth');
+$router->get('/post/edit', 'controllers/posts/edit.php')->middleware('auth');
 
-$router->get('/register', 'controllers/registration/create.php');
-$router->post('/register', 'controllers/registration/store.php');
+$router->get('/register', 'controllers/registration/create.php')->middleware('guest');
+$router->post('/register', 'controllers/registration/store.php')->middleware('guest');
